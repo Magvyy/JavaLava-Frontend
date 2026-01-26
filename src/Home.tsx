046c607ea803f032ab540e791a3bdd8c666d8578
Markus Adrian Halsvik <markus.halsvik@no.experis.com>
2026-01-26 11:29:05 +0100

    Simple home page now in order, adding scrolling next

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react'
 import './Home.css'
 
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 interface Post {
   content: string,
   published: string,
@@ -18,32 +28,68 @@ interface PostState {
 }
 
 export default function Secret() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const { posts, state } = usePostData();
   
+  if ( state.loading ) {
+    return (
+      <p>Loading...</p>
+    )
+  }
 
   return (
-    <></>
+    <div id="posts-container">
+      {posts.map(post => (
+        <Post
+          key={post.post_id}
+          post={post}
+        />
+      ))}
+    </div>
   )
 }
 
+interface PostProps {
+  post: Post
+}
+
+function Post(post: PostProps) {
+  let data = post.post;
+  const author = data.user_id;
+  const like_count = data.like_count;
+  const comment_count = data.comment_count;
 
 
+  return (
+    <Card className="mx-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>Small Card</CardTitle>
+        <CardDescription>
+          {data.user_name}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{data.content}</p>
+      </CardContent>
+      <CardFooter>
+        {data.published}
+      </CardFooter>
+    </Card>
+  )
+}
 
-
-export const usePostData = (postId: number | null) => {
+export const usePostData = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
     const [state, setState] = useState<PostState>({
         loading: true,
         error: null
     });
 
-    const [posts, setPosts] = useState<Post>([]);
-
     useEffect(() => {
-        const fetchPostData = async () => {
+        const fetchPosts = async () => {
             try {
                 let token = localStorage.getItem("jwt");
                 const response = await
-                    fetch("http://localhost:8080/post/" + postId, {
+                    fetch("http://localhost:8080/post/all?page=0", {
                         method: "GET",
                         headers: {
                             "Content-Type": "application/json",
@@ -53,7 +99,6 @@ export const usePostData = (postId: number | null) => {
                     });
                 if (response.ok) {
                     const postsJSON = await response.json();
-                    console.log(postsJSON);
                     setPosts(postsJSON);
                     setState({
                         loading: false,
@@ -66,11 +111,7 @@ export const usePostData = (postId: number | null) => {
                 setState({ loading: false, error: err.message });
             }
         }
-        if (postId) {
-            fetchPostData();
-        } else {
-            setState({ loading: false, error: "Can't view post with null id" });
-        }
+        fetchPosts();
     }, []);
     return { posts, state };
 }
