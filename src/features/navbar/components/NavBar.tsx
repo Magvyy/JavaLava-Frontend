18fb8c3c173c3efcf06e12788714ba852dcb532f
Magvyy <MAHImprovement@gmail.com>
2026-02-06 15:34:38 +0100

    minimal navigation bar

--- src/features/navbar/components/NavBar.tsx
+++ src/features/navbar/components/NavBar.tsx
@@ -8,28 +8,43 @@ import {
 } from "@/components/ui/navigation-menu";
 
 import "./css/navbar.css";
+import { useAuthenticateMe } from "@/shared/hooks/useAuthenticateMe";
+import { NavBarUserAccount } from "./NavBarUserAccount";
 
 interface NavBarProps {
     
 }
 export function NavBar() {
-	const localId = localStorage.getItem("user_id");
+    const { user, state } = useAuthenticateMe();
+
+    if (state.loading) {
+        return (
+            <NavigationMenu id="navbar">
+                Loading...
+            </NavigationMenu>
+        )
+    }
 
     return (
-        <NavigationMenu id="navbar">
-            <NavigationMenuList>
+        <NavigationMenu>
+            <NavigationMenuList className="justify-between w-screen px-9">
                 <NavigationMenuItem>
                     <NavigationMenuLink href="/">Home</NavigationMenuLink>
                 </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink href={"/user/" + localId}>Profile</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink href="/login">Login</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink href="/register">Register</NavigationMenuLink>
-                </NavigationMenuItem>
+                {(user) ? (
+                    <NavBarUserAccount
+                        user={user}
+                    />
+                ) : (
+                    <div className="flex flex-1 justify-end">
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/login">Login</NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink href="/register">Register</NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </div>
+                )}
             </NavigationMenuList>
         </NavigationMenu>
     )
--- src/features/users/components/User.tsx
+++ src/features/users/components/User.tsx
@@ -4,19 +4,24 @@ import "./user.css";
 import user_img from "./assets/user.svg";
 
 interface UserProps {
-  user: UserResponse
+  user: UserResponse,
+  idName?: string,
 }
-export function User({ user }: UserProps) {
+export function User({ user, idName }: UserProps) {
 
     const onClick = () => {
         window.location.href = "/user/" + user.id;
     }
 
     return (
-        <div className="user-container" onClick={(event) => {
-            event.stopPropagation();
-            onClick();
-        }}>
+        <div 
+            {...(idName && { id: idName })}
+            className="user-container"
+            onClick={(event) => {
+                event.stopPropagation();
+                onClick();
+            }}
+        >
             <div>
                 <img src={user_img}/>
                 <p>{user.user_name}</p>
