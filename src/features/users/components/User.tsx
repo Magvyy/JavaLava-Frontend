c0dcd3c3f1c6b1701a0690ef14520d71dcac04e6
Magvyy <MAHImprovement@gmail.com>
2026-02-09 11:30:07 +0100

    refactored to use tailwind instead of css

--- src/features/users/components/User.tsx
+++ src/features/users/components/User.tsx
@@ -1,24 +1,24 @@
 import type { UserResponse } from "@/types/ApiResponses";
-import "./user.css";
 
 import user_img from "./assets/user.svg";
 
 interface UserProps {
   user: UserResponse
+  className?: string
 }
-export function User({ user }: UserProps) {
+export function User({ user, className }: UserProps) {
 
     const onClick = () => {
         window.location.href = "/user/" + user.id;
     }
 
     return (
-        <div className="user-container" onClick={(event) => {
+        <div onClick={(event) => {
             event.stopPropagation();
             onClick();
         }}>
-            <div>
-                <img src={user_img}/>
+            <div className={className ? className : "p-0 pt-[10px] m-0 h-full flex flex-row items-center gap-[5px]"}>
+                <img className="h-[30px] rounded-[50%]" src={user_img}/>
                 <p>{user.user_name}</p>
             </div>
         </div>
