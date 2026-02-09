231b32ebda9f5b546e793b2f54b60824502682b4
Magvyy <MAHImprovement@gmail.com>
2026-02-09 11:23:58 +0100

    added endpoints to routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 import { HomePage } from '@/pages/feeds/HomePage';
 import { LoginPage } from '@/pages/auth/LoginPage';
 import { RegisterPage } from '@/pages/auth/RegisterPage';
-import { PostPage } from '@/pages/posts/PostPage';
+import { ReadPostPage } from '@/pages/posts/ReadPostPage';
 import { UserPage } from '@/pages/user-profile/UserPage';
 import { useAuthenticateMe } from '@/shared/hooks/useAuthenticateMe';
+import { EditPostPage } from '@/pages/posts/EditPostPage';
+import { CreatePostPage } from '@/pages/posts/CreatePostPage';
 
 export default function App() {
   const { user } = useAuthenticateMe();
@@ -23,9 +25,11 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/post/:id" element={<PostPage />} />
         <Route path="/profile" element={<UserPage />} />
         <Route path="/user/:userId" element={<UserPage />} />
+        <Route path="/post/:id" element={<ReadPostPage />} />
+        <Route path="/post/edit/:id" element={<EditPostPage />} />
+        <Route path="/post/create" element={<CreatePostPage />} />
       </Routes>
     </BrowserRouter>
   )
