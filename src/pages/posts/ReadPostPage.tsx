a8f03bfdd9f244be5f636f516a7c04ad0f61cf12
Magvyy <MAHImprovement@gmail.com>
2026-02-09 11:20:51 +0100

    added pages for editing and creating posts

---
+++ src/pages/posts/ReadPostPage.tsx
@@ -0,0 +1,53 @@
+import { ReadPost} from "@/features/posts";
+import { usePost } from "./hooks/usePost";
+import { usePostComments } from "./hooks/usePostComments";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { PostHeader } from "@/features/posts/components/PostHeader";
+import { PostContentReader } from "@/features/posts/components/read/PostContentReader";
+import { PostFooterReader } from "@/features/posts/components/read/PostFooterReader";
+
+
+
+export function ReadPostPage() {
+    const { id } = useParams<{ id: string }>();
+    
+    if (!id) {
+        window.location.href = "/";
+    }
+
+    const { post, setPost } = usePost(Number(id));
+    const { comments, setComments } = usePostComments(Number(id));
+
+    const onDelete = (id: number) => {
+        window.location.href = "/";
+    }
+
+    useEffect(() => {
+    }, [post, comments]);
+
+    return (
+        <ReadPost
+            post={post}
+            headerChild={
+                <PostHeader
+                    post_id={post.id}
+                    onDelete={onDelete}
+                    user={post.user}
+                />
+            }
+            contentChild={
+                <PostContentReader
+                    post={post}
+                />
+            }
+            footerChild={
+                <PostFooterReader
+                    post_id={post.id}
+                    liked={post.liked}
+                    comments={comments}
+                />
+            }
+        />
+    )
+}
