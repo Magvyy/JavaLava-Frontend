36d86a17809c388bb1d09b6a4b07c97d8c4ff9cf
Magvyy <MAHImprovement@gmail.com>
2026-02-11 08:28:19 +0100

    Implemented a component encompassing conversations displayed on the sidebar

---
+++ src/features/sidebar/components/SideBarConversations.tsx
@@ -0,0 +1,23 @@
+import { useConversations } from "../hooks/useConversations";
+import { useState } from "react";
+import { SideBarConversation } from "./SideBarConversation";
+
+interface SideBarConversationsProps {
+
+}
+
+export function SideBarConversations({  }: SideBarConversationsProps) {
+    const [update, setUpdate] = useState<boolean>(false);
+    const { conversations, setConversations, State } = useConversations(update);
+
+    return (
+        <div className="flex flex-col gap-[10px] w-full">
+            {conversations.map(conversation => (
+                <SideBarConversation
+                    key={conversation.id}
+                    message={conversation}
+                />
+            ))}
+        </div>
+    )
+}
