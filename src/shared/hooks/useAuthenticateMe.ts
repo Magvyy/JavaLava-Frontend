71eebf822c01580ce000c5d1c74d2472ddbbd3c6
Magvyy <MAHImprovement@gmail.com>
2026-02-11 13:18:21 +0100

    Updated to work with custom hook

--- src/shared/hooks/useAuthenticateMe.ts
+++ src/shared/hooks/useAuthenticateMe.ts
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import type { UserResponse } from "../types/UserApi";
 import { useApiCall } from "./useApiCall";
 
 export const useAuthenticateMe = () => {
-    const state = useApiCall<UserResponse>({
+    const [submit, setSubmit] = useState<boolean>(false);
+    const { state, handleApiCall } = useApiCall<UserResponse>({
         endpoint: "http://localhost:8080/auth/me",
         credentials: true,
         method: "GET",
     });
-    return { state };
+    if (!submit) {
+        handleApiCall();
+        setSubmit(true);
+    }
+    return { user: state.result?.data, state };
 }
