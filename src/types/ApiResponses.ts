7b05c5265a2501566f13bdcf85447d1cd3af7ad6
Magvyy <MAHImprovement@gmail.com>
2026-02-11 08:25:24 +0100

    set up message response and request types

--- src/types/ApiResponses.ts
+++ src/types/ApiResponses.ts
@@ -1,11 +1,11 @@
 
 export interface State {
-  loading: boolean,
+  loading: boolean
   error: string | null
 }
 
 export interface UserResponse {
-  id: number,
+  id: number
   user_name: string
 }
 
@@ -14,35 +14,49 @@ export interface ProfileUserResponse extends UserResponse {
 }
 
 export interface PostResponse {
-  id: number,
-  user: UserResponse,
-  liked: boolean,
-  content: string,
-  published: string,
-  visible: boolean,
-  like_count: number,
+  id: number
+  user: UserResponse
+  liked: boolean
+  content: string
+  published: string
+  visible: boolean
+  like_count: number
   comment_count: number
 }
 
 export interface PostRequest {
-  id: number | null,
-  content: string,
-  published: string,
+  id: number | null
+  content: string
+  published: string
   visible: boolean
 }
 
 export interface CommentResponse {
-  id: number,
-  user: UserResponse,
-  content: string,
-  published: string,
-  post_id: number,
-}
-
-export interface CommentRequest {
-  id: number | null,
-  content: string,
-  published: string,
+  id: number
+  user: UserResponse
+  content: string
+  published: string
   post_id: number
 }
 
+export interface CommentRequest {
+  id: number | null
+  content: string
+  published: string
+  post_id: number
+}
+
+export interface MessageResponse {
+  id: number
+  from: UserResponse
+  to: UserResponse
+  content: string
+  sent: string
+}
+
+export interface MessageRequest {
+  id: number | null
+  to_user_id: number
+  content: string
+  sent: string
+}
