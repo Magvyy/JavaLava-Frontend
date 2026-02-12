0440319d64f10369ac15987588ac43587fd82f7b
Magvyy <MAHImprovement@gmail.com>
2026-02-12 14:16:55 +0100

    Gave this page center-sidebar class and set min width

--- src/pages/feeds/HomePage.tsx
+++ src/pages/feeds/HomePage.tsx
@@ -61,7 +61,7 @@ export function HomePage() {
     return (
         <Loader state={state} className="w-full h-[200px] p-4">
             {(posts) => 
-                <div className="w-2/5 flex flex-col justify-center items-center gap-[20px] min-w-[200px]">
+                <div className="w-full p-5 flex flex-col justify-center items-center gap-[20px] min-w-[200px] center-sidebar">
                     {posts.map(post => (
                         <ReadPost key={post.id} post={post} onClick={onClickPost}>
                             <PostHeader
