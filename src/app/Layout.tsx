4acd45195b3873f7cc81659f19d5b595a6614f64
Magvyy <MAHImprovement@gmail.com>
2026-02-12 14:09:54 +0100

    Minor tailwind change

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { NavBar } from "@/features/navbar";
 import { AppSideBar } from "@/features/sidebar";
 
