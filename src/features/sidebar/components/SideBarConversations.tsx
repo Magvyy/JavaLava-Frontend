9f5275bf81b384ca20fcb0442e7a3f93782f184e
Magvyy <MAHImprovement@gmail.com>
2026-02-13 09:11:47 +0100

    id of sidebarconversation component is now the user id of the other user

--- src/features/sidebar/components/SideBarConversations.tsx
+++ src/features/sidebar/components/SideBarConversations.tsx
@@ -1,3 +1,4 @@
+import { useAuth } from "@/contexts/AuthContext";
 import { useConversations } from "../hooks/useConversations";
 import { SideBarConversation } from "./SideBarConversation";
 import { Loader } from "@/shared/components/Loader";
@@ -7,18 +8,20 @@ interface SideBarConversationsProps {
 }
 
 export function SideBarConversations({  }: SideBarConversationsProps) {
-    const { state } = useConversations();
+    const { authUser, authState } = useAuth();
+    const { state, conversations } = useConversations();
 
     return (
-        <Loader state={state} className="w-full p-4">
+        <Loader state={state} data={conversations} className="w-full p-4">
             {(conversations) => 
                 <div className="flex flex-col gap-[10px] w-full">
-                    {conversations.map(conversation => (
+                    {conversations.map(conversation => 
                         <SideBarConversation
-                            key={conversation.id}
+                            key={(authUser && authUser.id == conversation.from.id) ? conversation.to.id : conversation.from.id}
                             message={conversation}
                         />
-                    ))}
+                    )
+                    }
                 </div>
             }
         </Loader>
