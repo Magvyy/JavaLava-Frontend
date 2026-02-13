73dd2b2d883929ff9b237cb4f419b88c968a6982
Magvyy <MAHImprovement@gmail.com>
2026-02-13 09:38:44 +0100

    Wrapping app in query client provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,26 +10,31 @@ import { CreatePostPage } from '@/pages/posts/CreatePostPage';
 import Layout from './Layout';
 import Secret from '@/features/secret/Secret';
 import { Conversation } from '@/pages/conversation/Conversation';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <div id="page-content" className="w-full h-full flex-1">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/profile" element={<UserPage />} />
-            <Route path="/user/:userId" element={<UserPage />} />
-            <Route path="/post/:id" element={<ReadPostPage />} />
-            <Route path="/post/edit/:id" element={<EditPostPage />} />
-            <Route path="/post/create" element={<CreatePostPage />} />
-            <Route path="/empty" element={<Secret />} />
-            <Route path="/conversation/:id" element={<Conversation />} />
-          </Routes>
-        </div>
-      </Layout>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout>
+          <div id="page-content" className="w-full h-full flex-1">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/profile" element={<UserPage />} />
+              <Route path="/user/:userId" element={<UserPage />} />
+              <Route path="/post/:id" element={<ReadPostPage />} />
+              <Route path="/post/edit/:id" element={<EditPostPage />} />
+              <Route path="/post/create" element={<CreatePostPage />} />
+              <Route path="/empty" element={<Secret />} />
+              <Route path="/conversation/:id" element={<Conversation />} />
+            </Routes>
+          </div>
+        </Layout>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
