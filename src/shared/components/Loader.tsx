1fb3a5d8ed4fa0b8552d452ee3807750c1d927f7
Magvyy <MAHImprovement@gmail.com>
2026-02-16 09:31:56 +0100

    Improved loader to load new data in case of paginated data, as well as defining the relative location of the new data

--- src/shared/components/Loader.tsx
+++ src/shared/components/Loader.tsx
@@ -7,18 +7,49 @@ interface LoaderProps <T> {
     state: ApiState<T>
     data?: T
     children: (data: T) => ReactNode
+    upwards?: boolean
     className?: string
 }
-export function Loader <T> ({ state, data, children, className }: LoaderProps <T>) {
+export function Loader <T> ({ state, data, children, upwards, className }: LoaderProps <T>) {
     if (!state.called || state.loading) return (
-        <div className={className ? "flex justify-center items-center " + className : "flex justify-center items-center"}>
-            <TailSpin
-                height="40"
-                width="40"
-                color="#4fa94d"
-                ariaLabel="loading"
-            />
-        </div>
+        <>
+            {data ? (
+                upwards ? (
+                    <>
+                        <div className={className ? "flex justify-center items-center " + className : "flex justify-center items-center"}>
+                            <TailSpin
+                                height="40"
+                                width="40"
+                                color="#4fa94d"
+                                ariaLabel="loading"
+                            />
+                        </div>
+                        {children(data)}
+                    </>
+                ) : (
+                    <>
+                        {children(data)}
+                        <div className={className ? "flex justify-center items-center " + className : "flex justify-center items-center"}>
+                            <TailSpin
+                                height="40"
+                                width="40"
+                                color="#4fa94d"
+                                ariaLabel="loading"
+                            />
+                        </div>
+                    </>
+                )
+            ) : (
+                <div className={className ? "flex justify-center items-center " + className : "flex justify-center items-center"}>
+                    <TailSpin
+                        height="40"
+                        width="40"
+                        color="#4fa94d"
+                        ariaLabel="loading"
+                    />
+                </div>
+            )}
+        </>
     )
 
     const result = state.result;
