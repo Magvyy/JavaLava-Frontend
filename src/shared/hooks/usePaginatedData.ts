156ce2670c03926df488d15526b9c256dd4e759f
Magvyy <MAHImprovement@gmail.com>
2026-02-18 08:37:54 +0100

    coupled these two hooks since they're always used together

--- src/shared/hooks/usePaginatedData.ts
+++ src/shared/hooks/usePaginatedData.ts
@@ -1,31 +1,54 @@
 import { useApiCall } from "@/shared/hooks/useApiCall";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Id } from "../types/Id";
 
 
-export const usePaginatedData = <T extends Id> (endpoint: string) => {
+export const usePaginatedData = <T extends Id> (endpoint: string, reversed?: boolean, query?: string) => {
+    const [empty, setEmpty] = useState<boolean>(false);
     const [page, setPage] = useState<number>(0);
     const [data, setData] = useState<T[]>([]);
     const { state, handleApiCall } = useApiCall<T[]>();
 
+    const added = useRef(false);
+
     useEffect(() => {
+        if (query && query === "") return;
         handleApiCall({
-            endpoint: endpoint + "?page=" + page,
+            endpoint: endpoint + "?page=" + page + (query ? "&q=" + query : ""),
             credentials: true,
             method: "GET",
         });
-    }, [page])
+        added.current = false;
+    }, [page, query])
     
     useEffect(() => {
-        const data = state.result?.data;
-        if (!data) return;
+        const addData = state.result?.data;
+        if (!addData) return;
+        if (addData.length === 0) {
+            setEmpty(true);
+            return;
+        }
 
         setData(prev => {
             const existingIds = new Set(prev.map(d => d.id));
-            const newItems = data.filter(item => !existingIds.has(item.id));
-            return [...prev, ...newItems];
+            const newItems = addData.filter(item => !existingIds.has(item.id));
+            
+            const newIds = new Set(addData.map(d => d.id));
+            const updatedItems = prev.map(item => {
+                if (newIds.has(item.id)) {
+                    return addData.find(i => i.id === item.id);
+                } else {
+                    return item;
+                }
+            })
+
+            if (reversed) {
+                return [...newItems, ...updatedItems as T[]];
+            }
+            added.current = true;
+            return [...updatedItems as T[], ...newItems];
         })
     }, [state.result?.data])
 
-    return { data, setData, page, setPage, state };
+    return { data, setData, page, setPage, state, empty, setEmpty, added };
 }
--- src/shared/hooks/useScrollToEnd.ts
+++ src/shared/hooks/useScrollToEnd.ts
@@ -1,17 +1,76 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useRef, type RefObject } from "react";
+import { usePaginatedData } from "./usePaginatedData";
+import type { Id } from "../types/Id";
 
 
-export const useScrollToEnd = (onEnd: () => void, offset = 0) => {
+export const useScrollToEnd = <T extends Id> (endpoint: string, ref: RefObject<HTMLElement | null>, bottom = true, offset = 0, reversed = false, query?: string) => {
+    const { data, setData, setPage, state, empty, setEmpty, added } = usePaginatedData<T>(endpoint, reversed, query);
+    
+    const scrollPosition = useRef(0);
+    const previousHeight = useRef(0);
+    const lastScrollTime = useRef(Date.now());
+    const interval = 500;
+
+    const reset = () => {
+        added.current = false;
+        setEmpty(false);
+        setPage(0);
+    }
+
+    const update = () => {
+        if (added.current) return;
+        setPage(prev => prev + 1);
+        lastScrollTime.current = Date.now();
+    }
+    
     useEffect(() => {
+        if (empty || !ref) return;
+        
+        const element = ref.current;
+        if (!element) return;
+        
         const handleScroll = () => {
-            const viewportBottom = window.scrollY + window.innerHeight;
-            const documentHeight = document.documentElement.scrollHeight;
+            if (state.loading) return;
 
-            if (viewportBottom >= documentHeight - offset) {
-                onEnd();
+            if (bottom) {
+                const scrollBottom = element.scrollTop + element.clientHeight;
+                const scrollHeight = element.scrollHeight;
+                if ((scrollBottom >= scrollHeight - (offset + 2)) && (Date.now() - lastScrollTime.current) > interval) {
+                    update();
+                    scrollPosition.current = element.scrollTop;
+                }
+            } else {
+                const scrollTop = element.scrollTop;
+                if (scrollTop <= (offset + 2) && (Date.now() - lastScrollTime.current) > interval) {
+                    update();
+                    previousHeight.current = element.scrollHeight;
+                }
             }
         }
-        window.addEventListener("scroll", handleScroll, { passive: true });
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [onEnd, offset]);
+        element.addEventListener("scroll", handleScroll, { passive: true });
+        return () => element.removeEventListener("scroll", handleScroll);
+    }, [ref.current, offset, state, empty]);
+    
+    useEffect(() => {
+        const element = ref.current;
+        if (!element || state.loading) return;
+
+        const outerHeight = element.clientHeight;
+        const innerHeight = element.scrollHeight;
+        if (innerHeight <= outerHeight) {
+            update();
+        }
+    }, [data, ref.current])
+        
+    useLayoutEffect(() => {
+        const element = ref.current;
+        if (!element) return;
+        if (bottom) {
+            element.scrollTop = scrollPosition.current;
+        } else {
+            element.scrollTop = element.scrollHeight - previousHeight.current;
+        }
+    });
+
+    return { data, setData, state, reset }
 }
