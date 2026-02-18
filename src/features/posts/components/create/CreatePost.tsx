b324954adc49250a6bdef7264b7824f2817e501a
Magvyy <MAHImprovement@gmail.com>
2026-02-18 08:54:05 +0100

    Changed width of components

--- src/features/posts/components/create/CreatePost.tsx
+++ src/features/posts/components/create/CreatePost.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { type ReactNode, type RefObject } from "react";
 import { Card } from "@/components/ui/card";
 
 
@@ -7,11 +7,12 @@ interface CreatePostProps {
   contentChild: ReactNode
   footerChild: ReactNode
   className?: string
+  ref?: RefObject<HTMLDivElement | null>
 }
-export function CreatePost({ onError, contentChild, footerChild, className }: CreatePostProps) {
+export function CreatePost({ onError, contentChild, footerChild, className, ref }: CreatePostProps) {
 
   return (
-    <Card className={className ? className : "w-2/5 p-0 min-w-[200px]"}>
+    <Card className={className ? className : "w-2/5 p-0 min-w-[350px]"} ref={ref}>
       {contentChild}
       {footerChild}
     </Card>
