35ffd36bd5ff29c18855b3ca34492b5089753234
Magvyy <MAHImprovement@gmail.com>
2026-02-25 08:27:45 +0100

    hotfixed issue with sidebar conversation profile picture size

--- src/features/sidebar/components/SideBarConversation.tsx
+++ src/features/sidebar/components/SideBarConversation.tsx
@@ -27,10 +27,10 @@ export function SideBarConversation({ message }: SideBarConversationProps) {
                     }}
                 />
             </div>
-            <div className="flex flex-col gap-[5px] w-full">
+            <div className="flex flex-col gap-[5px] w-full flex-1">
                 <p className="flex justify-start items-center">{friendData.user_name}</p>
                 <div className="flex justify-between">
-                    <p className="flex items-center">{message.content}</p>
+                    <p className="flex items-center text-[12px]">{message.content}</p>
                     <p className="flex items-center text-[8px]">{timeSince}</p>
                 </div>
             </div>
