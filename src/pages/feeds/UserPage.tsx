dfdd35a98f3cb7f5aa2e420012368c8509c74a47
Magvyy <MAHImprovement@gmail.com>
2026-02-25 08:13:56 +0100

    Updated user profile to refresh when sending friend request

--- src/pages/feeds/UserPage.tsx
+++ src/pages/feeds/UserPage.tsx
@@ -1,4 +1,4 @@
-import { useRef, type MouseEvent } from "react";
+import { useEffect, useRef, useState, type MouseEvent } from "react";
 import { useParams } from "react-router-dom";
 import { useScrollToEnd } from "@/shared/hooks/useScrollToEnd";
 import { useProfileUser } from "./hooks/useProfileUser";
@@ -11,6 +11,10 @@ import { PostFooterReader } from "@/features/posts/components/read/PostFooterRea
 import type { PostResponse } from "@/shared/types/PostApi";
 import { useAuth } from "@/contexts/AuthContext";
 import { Loader } from "@/shared/components/Loader";
+import { FileInput } from "@/features/filedrop/components/FileInput";
+import { Button } from "@/components/ui/button";
+import { updateProfileApi } from "@/shared/services/users/updateProfileApi";
+import type { ProfileUserResponse, UserRequest, UserResponse } from "@/shared/types/UserApi";
 
 export function UserPage() {
 	const { userId } = useParams();
@@ -27,22 +31,8 @@ export function UserPage() {
 		containerRef
 	);
 
-	const { profileUser, setProfileUser, profileLoading, profileError: error } = useProfileUser(profileId);
+	const { state: profileState, refetch } = useProfileUser(profileId);
 
-	const onCreate = (post: PostResponse) => {
-        setPosts([post, ...posts]);
-    }
-
-	const onEdit = (edit: PostResponse) => {
-		const temp = posts.map(post => {
-			if (post.id === edit.id) {
-				return edit;
-			} else {
-				return post;
-			}
-		});
-		setPosts(temp);
-	};
 
 	const onDelete = (del: PostResponse | number) => {
 		const temp = posts.filter(post => {
@@ -59,87 +49,105 @@ export function UserPage() {
         window.location.href = "/posts/" + post.id;
     }
 
+	const [file, setFile] = useState<File | undefined>(undefined);
 
-	const showEmptyState = !state.loading && posts.length === 0 && profileUser != null;
-	const showLoadingState = state.loading && posts.length === 0;
+	const uploadProfilePicture = async (user: UserRequest) => {
+		await updateProfileApi(user, file);
+		window.location.href = "/user/" + user.id;
+	}
+
+	const makeUserRequest = (profile: ProfileUserResponse): UserRequest => {
+		return {
+			id: profile.id,
+			user_name: profile.user_name
+		}
+	}
 	
 	const profileHeader = (
-	<div className="w-4/5 flex flex-col items-center justify-between gap-[16px]">
-		{profileUser &&
-			<>
-				<User
-					user={profileUser}
-					profilePicChild={
-						<ProfilePic
-							className="w-[200px] h-[200px] rounded-[50%]"
-							onClick={(e: MouseEvent<HTMLImageElement>) => {
-								e.stopPropagation();
-							}}
+		<div className="w-4/5 flex flex-col items-center justify-between gap-[16px]">
+			<Loader state={profileState}>
+				{(profileUser) => 
+					<>
+						<User
+							user={profileUser}
+							profilePicChild={
+								<ProfilePic
+									className="w-[200px] h-[200px] rounded-[50%]"
+									onClick={(e: MouseEvent<HTMLImageElement>) => {
+										e.stopPropagation();
+									}}
+									user={profileUser}
+								/>
+							}
+							className="flex flex-col gap-[10px] text-center"
 						/>
-					}
-					className="flex flex-col gap-[10px] text-center"
-				/>
-				<FriendActions
-					profileUser={profileUser}
-					isSelf={isSelf}
-					onFriendStatusChange={(status) =>
-						setProfileUser({ ...profileUser, friend_status: status })
-					}
-					onVisibilityChange={() => {
-						reset();
-					}}
-				/>
-			</>
-		}
-	</div>
-	);
-
-	const profileState = (
-        <div className="w-4/5 text-left">
-            {profileLoading && <p>Loading profile...</p>}
-            {!profileLoading && error === "not-found" && <p>User does not exist.</p>}
-            {showEmptyState && <p>@{profileUser?.user_name} hasn’t posted yet.</p>}
-        </div>
-    );
-
-	const profileFeed = (
-		<>
-			<Loader state={state} data={posts} className="w-2/5 p-0 min-w-[350px]">
-				{(posts, spinner) => 
-					<div
-						className="h-full p-5 flex flex-col items-center gap-[20px] min-w-[200px] overflow-auto scrollbar-hide"
-						ref={containerRef}
-					>
-							{profileHeader}
-							{profileState}
-							{posts.map(post => (
-								<ReadPost
-									key={post.id}
-									post={post}
-									onClick={onClickPost}
-									className="w-full p-0 min-w-[350px]"
+						{isSelf &&
+							<>
+								<FileInput
+									className="w-full h-full flex text-center gap-[5px]"
+									file={file}
+									setFile={setFile}
+									description="Upload profile picture"
+								/>
+								<Button
+									onClick={() => uploadProfilePicture(makeUserRequest(profileUser))}
 								>
-									<PostHeader
-										postId={post.id}
-										onDelete={onDelete}
-										user={post.user}
-									/>
-									<PostContentReader
-										post={post}
-									/>
-									<PostFooterReader
-										postId={post.id}
-										liked={post.liked}
-										likeCount={post.like_count}
-										commentCount={post.comment_count}
-									/>
-								</ReadPost>
-							))}
-						{spinner}
-					</div>
+									Upload
+								</Button>
+							</>
+						}
+						{!isSelf && 
+							<FriendActions
+								profileUser={profileUser}
+								onFriendStatusChange={(status) =>
+									refetch()
+								}
+								onVisibilityChange={() => {
+									reset();
+								}}
+							/>
+						}
+					</>
 				}
 			</Loader>
-		</>
+		</div>
+	);
+
+	const profileFeed = (
+		<Loader state={state} data={posts}>
+			{(posts, spinner) => 
+				<div
+					className="h-full p-5 flex flex-col items-center gap-[20px] min-w-[200px] overflow-auto scrollbar-hide"
+					ref={containerRef}
+				>
+						{profileHeader}
+						{posts.map(post => (
+							<ReadPost
+								key={post.id}
+								post={post}
+								onClick={onClickPost}
+								className="w-full p-0 min-w-[350px]"
+							>
+								<PostHeader
+									postId={post.id}
+									onDelete={onDelete}
+									user={post.user}
+								/>
+								<PostContentReader
+									post={post}
+								/>
+								<PostFooterReader
+									postId={post.id}
+									liked={post.liked}
+									likeCount={post.like_count}
+									commentCount={post.comment_count}
+								/>
+							</ReadPost>
+						))}
+					{spinner}
+				</div>
+			}
+		</Loader>
 	);
 
 	return (
